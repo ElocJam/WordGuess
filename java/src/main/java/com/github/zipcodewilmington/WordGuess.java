b7c608455c4d8ed76e957644080c0b9fb280af97
cjamison <elocjam13@gmail.com>
2025-11-03 14:27:17 -0500

    Updated and Working with 6 lives

--- java/src/main/java/com/github/zipcodewilmington/WordGuess.java
+++ java/src/main/java/com/github/zipcodewilmington/WordGuess.java
@@ -13,61 +13,41 @@ public class WordGuess
     public static void main(String [] args) {
         System.out.println("Welcome to WordGuess!");
         String secretWord = "cow";
-        int userGuessesNum = 0;
-        int userLives = secretWord.length() + 3;
+        int userGuessesNum = 1;
+        int userLives = 6;
         Scanner scanner = new Scanner(System.in);
+        var wrongGuesses = new ArrayList<String>();
 
     while (true) {
         System.out.println("This is guess number " + userGuessesNum);
         System.out.println("Secret word is: _ _ _");
-        System.out.println("Wrong guesses: ");
+        System.out.println("Wrong Guesses: " + wrongGuesses);
         System.out.println("Enter a letter? (or quit)");
         String userInput = scanner.nextLine();
         System.out.println("User typed: " + userInput);
+
+        if (userInput.equals("cow")) {
+            System.out.println("You are a weiner!");
+            break;
+        }
+
         System.out.println("Lives left " + userLives);
-                
+
         if (userLives == 0.0) {
             System.out.println("You Died, Loser!");
             break;
         }
-        
+
         if (userInput.equals("quit")) {
             break;
         }
 
         userGuessesNum++;
         userLives--;
+        wrongGuesses.add(userInput);
 
     }
     System.out.println("Goodbye");
     scanner.close();
     }
 }
-
-
-
-/* 
-// java version for jshell KRIS CODE
-import java.util.Scanner;
-
-System.out.println("Welcome to Word Guess!");
-String secretWord = "cat";
-int userGuessesNum = 0;
-Scanner scanner = new Scanner(System.in);
-
-// is infinite loop ok?
-while (true) {
-    System.out.print("Enter a letter? (or quit) ");
-    String userInput = scanner.nextLine();
-    System.out.println("User typed: " + userInput);
-    if (userInput.equals("quit")) {
-        break;
-    }
-    userGuessesNum++;
-    // handle the guess
-}
-
-System.out.println("Ba-Bye!");
-scanner.close();
-
-*/
