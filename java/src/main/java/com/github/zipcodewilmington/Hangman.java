80567e41847eea3d48841d0711339644943a90a0
cjamison <elocjam13@gmail.com>
2025-11-03 12:19:22 -0500

    Added Kris sample code

--- java/src/main/java/com/github/zipcodewilmington/Hangman.java
+++ java/src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -8,3 +8,31 @@ package com.github.zipcodewilmington;
  */
 public class Hangman {
 }
+
+
+
+/* 
+// java version for jshell KRIS CODE
+import java.util.Scanner;
+
+System.out.println("Welcome to Word Guess!");
+String secretWord = "cat";
+int userGuessesNum = 0;
+Scanner scanner = new Scanner(System.in);
+
+// is infinite loop ok?
+while (true) {
+    System.out.print("Enter a letter? (or quit) ");
+    String userInput = scanner.nextLine();
+    System.out.println("User typed: " + userInput);
+    if (userInput.equals("quit")) {
+        break;
+    }
+    userGuessesNum++;
+    // handle the guess
+}
+
+System.out.println("Ba-Bye!");
+scanner.close();
+
+*/
